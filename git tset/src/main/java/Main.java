87fa29f721df6e01ea9d7302b0c03f9f6ebcda78
manntohihi <ovs246133@kcsf2014.onmicrosoft.com>
2026-01-09 12:39:07 +0900

    こんにちは

--- git tset/src/main/java/Main.java
+++ git tset/src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	   public static void main(String[] args){
 	        System.out.println("Hello GItHub! from Eclipse");
 	        System.out.println("プッシュ成功したら嬉しいな！");
+	        System.out.println("2回目コミット");
 	}
 }
